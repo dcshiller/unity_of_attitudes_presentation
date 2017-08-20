09821f68cfbec636fc06adfe1d25d9dc65c76a38
dcshiller <dcshiller@gmail.com>
2017-08-20 11:08:37 -0400

    last old version

--- app/display.js
+++ app/display.js
@@ -1,11 +1,11 @@
 const R = require('ramda');
 const SlideShow = require('./slide_show.js')
 
-document.addEventListener('DOMContentLoaded',
-  function(){
-    var video = document.querySelector("#video_element"); navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia; if (navigator.getUserMedia) { navigator.getUserMedia({video: true}, handleVideo, videoError); } function handleVideo(stream) { video.src = window.URL.createObjectURL(stream); } function videoError(e) { }
-  }
-)
+// document.addEventListener('DOMContentLoaded',
+//   function(){
+//     var video = document.querySelector("#video_element"); navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia; if (navigator.getUserMedia) { navigator.getUserMedia({video: true}, handleVideo, videoError); } function handleVideo(stream) { video.src = window.URL.createObjectURL(stream); } function videoError(e) { }
+//   }
+// )
 
 document.addEventListener('keypress', R.bind(SlideShow.nextFrame, SlideShow) );
 
--- dist/bundle.js
+++ dist/bundle.js
@@ -4687,11 +4687,11 @@ module.exports = __webpack_require__(323);
 const R = __webpack_require__(27);
 const SlideShow = __webpack_require__(320)
 
-document.addEventListener('DOMContentLoaded',
-  function(){
-    var video = document.querySelector("#video_element"); navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia; if (navigator.getUserMedia) { navigator.getUserMedia({video: true}, handleVideo, videoError); } function handleVideo(stream) { video.src = window.URL.createObjectURL(stream); } function videoError(e) { }
-  }
-)
+// document.addEventListener('DOMContentLoaded',
+//   function(){
+//     var video = document.querySelector("#video_element"); navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia; if (navigator.getUserMedia) { navigator.getUserMedia({video: true}, handleVideo, videoError); } function handleVideo(stream) { video.src = window.URL.createObjectURL(stream); } function videoError(e) { }
+//   }
+// )
 
 document.addEventListener('keypress', R.bind(SlideShow.nextFrame, SlideShow) );
 
