5354ed11c391e02cbb8541675e40561f4cf99a41
dcshiller <dcshiller@gmail.com>
2017-08-27 14:29:47 -0400

    vertical scroll

--- app/display.js
+++ app/display.js
@@ -1,13 +1,37 @@
 const R = require('ramda');
 const SlideShow = require('./slide_show.js')
 
-// document.addEventListener('DOMContentLoaded',
-//   function(){
-//     var video = document.querySelector("#video_element"); navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia; if (navigator.getUserMedia) { navigator.getUserMedia({video: true}, handleVideo, videoError); } function handleVideo(stream) { video.src = window.URL.createObjectURL(stream); } function videoError(e) { }
-//   }
-// )
-
 document.addEventListener('keypress', R.bind(SlideShow.nextFrame, SlideShow) );
+
+nextTimes = [ 0, // Begin
+              5000, // Play
+              22000, // NonCog
+              380000, // Example
+              108000, // SMJ
+              140000, // Not
+              150000, // had
+              175000, // Double
+              200000, // Uncertainty
+              226000, // Problem
+              260000, // blank
+              291500, // skip
+              292000, // RTM 
+              376000, // PTM
+              426000, // Moral Concept
+              549000, // Blank
+              632500, // Scroll Down
+              633000, // Spandrels
+              662000, // Evo Rationale
+              752000, // Exaptation
+              840000, // Summary
+              850000, // Thanks
+              855000 // Thanks 2
+            ]
+
+
+for(time of nextTimes){
+  setTimeout(SlideShow.nextFrame.bind(SlideShow), time)
+}
 
 for (i = 0; i < 0; i++) {
   SlideShow.nextFrame();
--- app/slide_show.js
+++ app/slide_show.js
@@ -15,14 +15,24 @@ const hide = R.curry(addClass)('hidden');
 const show = R.curry(removeClass)('hidden');
 const invisiblize = function(ele){ ele.classList.add('invisible') }
 const visiblize = function(ele){ ele.classList.remove('invisible') }
+const dissolve = function(ele){ ele.classList.add('dissolved') }
+const recompose = function(ele){ ele.classList.remove('dissolved') }
 const focus = function(ele){ ele.classList.add('focused') }
 const unfocus = function(ele){ ele.classList.remove('focused') }
 const showNext = function(ele){ ele.querySelector('.hidden').classList.remove('hidden') }
+const showNextSlide = function(ele){ 
+  ele.querySelector('.focused + .focusable').classList.add('focused');
+  ele.querySelector('.focused').classList.remove('focused');
+  slideUp(ele);
+}
 const removeNextEll = function(ele){ ele.querySelector('.terminal_ellipsis').classList.remove('terminal_ellipsis') }
 var i = 0
 const resetI = function(){i = 0}
 const slideRight = function(ele){ 
   ele.style.transform = `translate(-${500 * ++i}px)`;
+  }
+const slideUp = function(ele){ 
+  ele.style.transform = `translate(0px, -${290 * ++i}px)`;
   }
 const sb = function(func){ return func.bind.bind(func) }
 const oneToArg = function(func) { return R.curryN(2, sb(func))(func); }
@@ -37,9 +47,13 @@ const makeNextShown = R.compose(oneToArg(showNext), el);
 const hideNextEll   = R.compose(oneToArg(removeNextEll), el);
 const makeInvisible = R.compose(oneToArg(invisiblize), el);
 const makeVisible   = R.compose(oneToArg(visiblize), el);
+const makeDissolved = R.compose(oneToArg(dissolve), el);
+const makeComposed  = R.compose(oneToArg(recompose), el);
 const makeFocused   = R.compose(oneToArg(focus), el);
 const makeUnfocused = R.compose(oneToArg(unfocus), el);
 const moveRight     = R.compose(oneToArg(slideRight), el);
+const moveUp        = R.compose(oneToArg(slideUp), el);
+const advanceSlide = R.compose(oneToArg(showNextSlide), el)
 
 SlideShow.addFrame(
   [empty],
@@ -51,158 +65,54 @@ SlideShow.addFrame(
   [makeHidden('#title_flash')]
 );
 
-SlideShow.addFrame(
-  [makeShown('#problem_flash')],
-  [makeHidden('#problem_flash')]
-);
+const nextSlide =  SlideShow.addFrame.bind(SlideShow, [advanceSlide('.slides')], [empty] )
+
 
 SlideShow.addFrame(
-  [makeShown('#main_problem')],
-  [makeHidden('#main_problem')]
-);
-
-SlideShow.addFrame(
-  [makeShown('#noncognitivism')],
-  [makeHidden('#noncognitivism')]
-);
-
-SlideShow.addFrame(
-  [makeShown('#attitudes')],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [
-    makeShown('#menagerie')
-  ],
-  [makeUnfocused('#logical')]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#miscellaneous'), moveRight('#menagerie')],
-  [makeUnfocused('#miscellaneous')]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#attitudinal'), moveRight('#menagerie')],
-  [makeHidden('#attitudes'), makeHidden("#menagerie")]
-);
-
-SlideShow.addFrame(
-  [makeShown('#solution_flash')],
-  [makeHidden('#solution_flash')]
-);
-
-SlideShow.addFrame(
-  [makeShown("#ptm")],[empty]
-);
-
-SlideShow.addFrame(
-  [
-    resetI,
-    makeShown('#properties')
-  ],
-  [makeUnfocused('#semantic')]
-);
-
-SlideShow.addFrame(
-  [
-    makeFocused('#orthographic'),
-    moveRight('#properties')
-  ],
-  [
-    makeUnfocused('#orthographic')
-  ]
-);
-SlideShow.addFrame(
-  [
-    makeFocused('#sentences'),
-    moveRight('#properties')
+  [makeShown('.slides'),
+  function(){ el('video').play() }
   ],
   [empty]
 );
 
+nextSlide(); // Noncog
+nextSlide(); // Example
+nextSlide(); // Straightforwrd
+nextSlide(); // Extinct Species
+nextSlide(); // Children
+nextSlide(); // Double Normativity
+nextSlide(); // Uncertainty
+nextSlide(); // Unity
+
 SlideShow.addFrame(
-  [
-    makeHidden('#sentences_plain'),
-    makeShown('#sentences_underlined')
-  ],
-  [makeUnfocused("#sentences")]
+  [makeDissolved('.slides')],
+  [makeComposed('.slides')]
+);
+nextSlide();
+
+nextSlide(); // Representational
+nextSlide(); // Presentational
+nextSlide(); // Moral Concepts
+
+SlideShow.addFrame(
+  [makeDissolved('.slides')],
+  [makeComposed('.slides')]
+);
+nextSlide();
+
+nextSlide(); // Spandrel
+nextSlide(); // Rationale
+nextSlide(); // Exaption Proposal
+nextSlide(); // Summary
+
+SlideShow.addFrame(
+  [makeShown('#thanks_flash'), makeShown("#back_flash")],
+  [makeHidden('#thanks_flash')]
 );
 
 SlideShow.addFrame(
-  [makeFocused('#jabberwocky'), moveRight('#properties')],
-  [makeUnfocused("#jabberwocky"), makeHidden("#properties")]
-);
-
-SlideShow.addFrame(
-  [resetI, makeHidden('#ptm'), makeShown("#recipe_semantics")],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [makeShown('#moral_recipe_semantics')],
-  [makeUnfocused("#moral_concepts")]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#spandrels'), moveRight("#moral_recipe_semantics")],
-  [makeUnfocused("#spandrels")]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#moral_wishes'), moveRight("#moral_recipe_semantics")],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [makeNextShown('#moral_wishes'), hideNextEll("#moral_wishes")],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [makeNextShown('#moral_wishes'), hideNextEll("#moral_wishes")],
-  [makeHidden("#moral_recipe_semantics"), makeHidden('#recipe_semantics')]
-);
-
-SlideShow.addFrame(
-  [makeShown('#evolution_flash')],
-  [makeHidden('#evolution_flash')]
-);
-
-SlideShow.addFrame(
-  [makeShown('#evolution'), resetI],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [makeShown('#exaptation')],
-  [makeUnfocused('#attitudes_sentiments')]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#recency'), moveRight('#exaptation')],
-  [makeUnfocused('#recency')]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#exaptation_proposal'), moveRight('#exaptation')],
-  [makeUnfocused('#exaptation_proposal')]
-);
-
-SlideShow.addFrame(
-  [makeFocused('#biproducts'), moveRight('#exaptation')],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [makeNextShown('#biproducts')],
-  [empty]
-);
-
-SlideShow.addFrame(
-  [makeNextShown('#biproducts'), hideNextEll("#biproducts")],
-  [empty]
+  [makeShown('#thanks_2_flash')],
+  [makeHidden('#thanks_2_flash')]
 );
 
 
