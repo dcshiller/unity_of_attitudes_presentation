cf55ed08d9deb0e333a5120b9d150462810dfee6
dcshiller <dcshiller@gmail.com>
2017-08-29 21:09:46 -0400

    content and timing tweaks

--- app/display.js
+++ app/display.js
@@ -3,15 +3,16 @@ const SlideShow = require('./slide_show.js')
 
 document.addEventListener('keypress', R.bind(SlideShow.nextFrame, SlideShow) );
 
-nextTimes = [ 0, // Begin
+nextTimes = [ 
+              0, // Begin
               5000, // Play
               22000, // NonCog
-              380000, // Example
+              37500, // Example
               108000, // SMJ
               140000, // Not
               150000, // had
               175000, // Double
-              200000, // Uncertainty
+              205000, // Uncertainty
               226000, // Problem
               260000, // blank
               291500, // skip
@@ -19,8 +20,8 @@ nextTimes = [ 0, // Begin
               376000, // PTM
               426000, // Moral Concept
               549000, // Blank
-              632500, // Scroll Down
-              633000, // Spandrels
+              610500, // Scroll Down
+              611000, // Spandrels
               662000, // Evo Rationale
               752000, // Exaptation
               840000, // Summary
